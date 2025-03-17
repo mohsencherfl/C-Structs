6c1800290052804fdf8bec293d7ca76159a6e12b
mohsen bashashi <45238123+mohsencherfl@users.noreply.github.com>
2025-03-17 17:47:29 +0330

    add a struct-type variable: "pointsArray[5]"
    This code snippet extends the program to handle an array of Point structures. It allows the user to input coordinates for five points and then displays them on the screen. The first for loop iterates through the pointsArray, prompting the user to input the x and y coordinates for each point. The second for loop iterates through the same array to print the coordinates for all points in a formatted style. This addition demonstrates the use of arrays of structures and iterative processing in C.

--- c-structs/main.c
+++ c-structs/main.c
@@ -27,8 +27,34 @@ Point inputPoint() {
 int main() {
 
         // Get a point coordinate from the user and print it on the screen.
+/*
     Point p1 = inputPoint();
     printPoint(p1);
+
+                                */
+
+
+        // Iterate over the array elements and assign a point coordinates for each element and
+        // then print the points coordinates on the screen.
+
+    Point pointsArray[5];
+
+    // User input values
+    for(int i=0; i<5; i++)
+    {
+        printf("Point #%d \n", i+1);
+        printf("Enter x coordinate: ");
+        scanf("%d", &pointsArray[i].x);
+        printf("Enter y coordinate: ");
+        scanf("%d", &pointsArray[i].y);
+    }
+    printf("\n");
+
+    // Print user input values
+    for(int j=0; j<5; j++)
+    {
+        printf("Point %d Coordinates ==> (%d, %d) \n", j+1, pointsArray[j].x, pointsArray[j].y);
+    }
 
     return 0;
 }
